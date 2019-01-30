7e973b0d4e920393f4ec65d86308fe6fbee73be0
Stephen Andryc <Stephen_Andryc@Stephens-MacBook-Air.local>
2019-01-30 09:51:42 -0500

    Some edits some procrastinating

--- models/User.js
+++ models/User.js
@@ -1,29 +1,23 @@
 var fs = require("fs");
 
 exports.getUser = function(user_id) {
-  console.log("Users.getUser: "+user_id);
+  console.log("User.getUser("+user_id+") called");
 
   var user = createBlankUser();
-  var all_users = getAllDatabaseRows();
+  var all_users = getRows();
 
   for(var i=1; i<all_users.length; i++){
-    var u = all_users[i].split(',');
-    if(u[0].trim()==user_id.trim()){
-      user={
-        name:u[0].trim(),
-        games_played:u[1].trim(),
-        lost:u[2].trim(),
-        won:u[3].trim(),
-        password:u[4].trim()
-      }
+    var u = parseString(all_users[i]);
+    if(u.name==user_id){
+      user=u;
     }
   }
-
+  console.log("JSON.stringify called on output object: "+JSON.stringify(user));
   return user;
 }
 
 exports.updateUser = function(user_id, new_info) {
-  console.log("Users.getUser");
+  console.log("Users.updateUser()");
   var user={
     name:"test"
   };
@@ -31,16 +25,22 @@ exports.updateUser = function(user_id, new_info) {
   return user;
 }
 
-var getAllDatabaseRows= function(){
+var getRows= function(){
+  console.log("User.getRows() called");
   return fs.readFileSync(__dirname +'/../data/users.csv', 'utf8').split('\n');
 }
-var createBlankUser= function(){
-  var user={
-    name:"test",
-    games_played:"test",
-    lost:"test",
-    won:"test",
-    password:"test"
-  };
-  return user;
+//reads the users.csv and converts it into an array
+
+var parseString= function (str){
+  console.log("User.parseString() called on: "+str);
+  var arr = str.split(',');
+  var output = {name:arr[0], games_played:arr[1], lost:arr[2], won:arr[3], password:arr[4]};
+  console.log("JSON.stringify() called on output object: "+JSON.stringify(output));
+  return output;
 }
+
+var createBlankUser= function(){
+  console.log("User.createBlankUser() called");
+  return {name:"", games_played:0, lost:0, won:0, password:""};
+}
+//creates a blank user object
